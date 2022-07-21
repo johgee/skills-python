6de7247741e6e3c32fd36a8a3d46f106719c138c
Grace Joh <graceeunaejoh@gmail.com>
2022-07-21 00:57:24 -0400

    basic2

---
+++ basic2.py
@@ -0,0 +1,41 @@
+# write a function that compute the factorial of given numbers 
+# input = 8, output = 40320
+
+# def fact(x): ##????
+#   if x == 0:
+#     return 1
+#   return x * fact(x-1)
+
+# x = int(input())
+# print(fact(x))
+
+# lines = [] # not work
+# while True: 
+#   s = input("hello world")
+#   if s:
+#     lines.append(s.upper())
+#   else:
+#     break
+# for sentence in lines:
+#   print(s)
+
+
+# ss = "make first letter big"
+# print(ss.title())
+
+# s = "Hello World"
+# x = s.upper() # HELLO WORLD
+# x = s.swapcase() # hELLO wORLD
+
+# print(len(s)) # 11 
+# print(x)
+# print(s[0]) # H
+
+# s1 = "   Space before and after    "
+# print(s1) #   Space before and after   
+# print(s1.strip()) #Space before and after
+
+
+# s2 = s.replace("H", "K")
+# print(s2) # Hello World
+
